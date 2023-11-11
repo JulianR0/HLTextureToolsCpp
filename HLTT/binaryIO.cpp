9116b158a754fa94bea9a1196650683981189c16
Giegue <jumarti_5@yahoo.com.ar>
2023-11-11 20:27:59 -0300

    Fix windows compilation.

--- HLTT/binaryIO.cpp
+++ HLTT/binaryIO.cpp
@@ -5,22 +5,22 @@
 namespace BIN
 {
 	// BinaryReader
-	uint8_t ReadUInt8(wxFileInputStream *stream) { uint8_t output; stream->Read(reinterpret_cast<char*>(&output), 1); return output; }
-	uint16_t ReadUInt16(wxFileInputStream *stream) { uint16_t output; stream->Read(reinterpret_cast<char*>(&output), 2); return output; }
-	uint32_t ReadUInt32(wxFileInputStream *stream) { uint32_t output; stream->Read(reinterpret_cast<char*>(&output), 4); return output; }
+	UINT8 ReadUInt8(wxFileInputStream *stream) { UINT8 output; stream->Read(reinterpret_cast<char*>(&output), 1); return output; }
+	UINT16 ReadUInt16(wxFileInputStream *stream) { UINT16 output; stream->Read(reinterpret_cast<char*>(&output), 2); return output; }
+	UINT32 ReadUInt32(wxFileInputStream *stream) { UINT32 output; stream->Read(reinterpret_cast<char*>(&output), 4); return output; }
 	
-	int8_t ReadInt8(wxFileInputStream *stream) { int8_t output; stream->Read(reinterpret_cast<char*>(&output), 1); return output; }
-	int16_t ReadInt16(wxFileInputStream *stream) { int16_t output; stream->Read(reinterpret_cast<char*>(&output), 2); return output; }
-	int32_t ReadInt32(wxFileInputStream *stream) { int32_t output; stream->Read(reinterpret_cast<char*>(&output), 4); return output; }
+	INT8 ReadInt8(wxFileInputStream *stream) { INT8 output; stream->Read(reinterpret_cast<char*>(&output), 1); return output; }
+	INT16 ReadInt16(wxFileInputStream *stream) { INT16 output; stream->Read(reinterpret_cast<char*>(&output), 2); return output; }
+	INT32 ReadInt32(wxFileInputStream *stream) { INT32 output; stream->Read(reinterpret_cast<char*>(&output), 4); return output; }
 	
 	float ReadFloat(wxFileInputStream *stream) { float output; stream->Read(reinterpret_cast<char*>(&output), 4); return output; }
 	
 	// BinaryWriter
-	void WriteUInt8(wxFileOutputStream *stream, uint8_t value) { stream->Write(reinterpret_cast<const char*>(&value), 1); }
-	void WriteUInt16(wxFileOutputStream *stream, uint16_t value) { stream->Write(reinterpret_cast<const char*>(&value), 2); }
-	void WriteUInt32(wxFileOutputStream *stream, uint32_t value) { stream->Write(reinterpret_cast<const char*>(&value), 4); }
+	void WriteUInt8(wxFileOutputStream *stream, UINT8 value) { stream->Write(reinterpret_cast<const char*>(&value), 1); }
+	void WriteUInt16(wxFileOutputStream *stream, UINT16 value) { stream->Write(reinterpret_cast<const char*>(&value), 2); }
+	void WriteUInt32(wxFileOutputStream *stream, UINT32 value) { stream->Write(reinterpret_cast<const char*>(&value), 4); }
 	
-	void WriteInt8(wxFileOutputStream *stream, int8_t value) { stream->Write(reinterpret_cast<const char*>(&value), 1); }
-	void WriteInt16(wxFileOutputStream *stream, int16_t value) { stream->Write(reinterpret_cast<const char*>(&value), 2); }
-	void WriteInt32(wxFileOutputStream *stream, int32_t value) { stream->Write(reinterpret_cast<const char*>(&value), 4); }
+	void WriteInt8(wxFileOutputStream *stream, INT8 value) { stream->Write(reinterpret_cast<const char*>(&value), 1); }
+	void WriteInt16(wxFileOutputStream *stream, INT16 value) { stream->Write(reinterpret_cast<const char*>(&value), 2); }
+	void WriteInt32(wxFileOutputStream *stream, INT32 value) { stream->Write(reinterpret_cast<const char*>(&value), 4); }
 }
--- HLTT/binaryIO.h
+++ HLTT/binaryIO.h
@@ -4,25 +4,32 @@
 
 #include <wx/wfstream.h>
 
+#ifdef _WIN32
+#include <basetsd.h>
+#endif
+#ifdef linux
+#include "linux.h"
+#endif
+
 namespace BIN
 {
 	// BinaryReader
-	uint8_t ReadUInt8(wxFileInputStream *stream);
-	uint16_t ReadUInt16(wxFileInputStream *stream);
-	uint32_t ReadUInt32(wxFileInputStream *stream);
+	UINT8 ReadUInt8(wxFileInputStream *stream);
+	UINT16 ReadUInt16(wxFileInputStream *stream);
+	UINT32 ReadUInt32(wxFileInputStream *stream);
 	
-	int8_t ReadInt8(wxFileInputStream *stream);
-	int16_t ReadInt16(wxFileInputStream *stream);
-	int32_t ReadInt32(wxFileInputStream *stream);
+	INT8 ReadInt8(wxFileInputStream *stream);
+	INT16 ReadInt16(wxFileInputStream *stream);
+	INT32 ReadInt32(wxFileInputStream *stream);
 	
 	float ReadFloat(wxFileInputStream *stream);
 	
 	// BinaryWriter
-	void WriteUInt8(wxFileOutputStream *stream, uint8_t value);
-	void WriteUInt16(wxFileOutputStream *stream, uint16_t value);
-	void WriteUInt32(wxFileOutputStream *stream, uint32_t value);
+	void WriteUInt8(wxFileOutputStream *stream, UINT8 value);
+	void WriteUInt16(wxFileOutputStream *stream, UINT16 value);
+	void WriteUInt32(wxFileOutputStream *stream, UINT32 value);
 	
-	void WriteInt8(wxFileOutputStream *stream, int8_t value);
-	void WriteInt16(wxFileOutputStream *stream, int16_t value);
-	void WriteInt32(wxFileOutputStream *stream, int32_t value);
+	void WriteInt8(wxFileOutputStream *stream, INT8 value);
+	void WriteInt16(wxFileOutputStream *stream, INT16 value);
+	void WriteInt32(wxFileOutputStream *stream, INT32 value);
 }
